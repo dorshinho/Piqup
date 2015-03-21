db41dd4d6093bbe33b2f1eba321d63f6f559bf59
Dorsh <dorshdeans@gmail.com>
2015-03-21 01:08:52 -0400

    sweep

--- js/Piqup.js
+++ js/Piqup.js
@@ -2,36 +2,6 @@
 (function(exports) {
     "use strict";
 
-    function scrollBodyTo(to, duration, callback) {
-        var start = window.scrollY,
-            change = to - start,
-            currentTime = 0,
-            increment = 20;
-
-        var animateScroll = function() {
-            currentTime += increment;
-            var val = Math.easeInOutQuad(currentTime, start, change, duration);
-            window.scrollTo(0, val);
-            if (currentTime <= duration) {
-                requestAnimationFrame(animateScroll);
-            } else {
-                callback && callback();
-            }
-        };
-        requestAnimationFrame(animateScroll);
-    }
-
-    //t = current time
-    //b = start value
-    //c = change in value
-    //d = duration
-    Math.easeInOutQuad = function(t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return c / 2 * t * t + b;
-        t--;
-        return -c / 2 * (t * (t - 2) - 1) + b;
-    };
-
 
     Backbone.PiqupRouter = Backbone.Router.extend({
 
@@ -84,12 +54,12 @@
     })
 
     Backbone.HomeView = Backbone.TemplateView.extend({
-        el: ".container",
+        el: ".yo",
         view: "home"
     })
 
     Backbone.SignUpView = Backbone.TemplateView.extend({
-        el: ".container",
+        el: ".yo",
         view: "login",
         events: {
             "click #login": "login"
--- js/TemplateView.js
+++ js/TemplateView.js
@@ -1,45 +1,42 @@
-;(function(exports){
+;(function(){
 
     "use strict";
 
-    exports.TemplateView = function createTemplateView(backboneOrParse, _, $){
-        return backboneOrParse.View.extend({
-            cache: {},
-            stream: function(url) {
-                var x = $.Deferred();
-                if (this.cache[url]) {
-                    x.resolve(this.cache[url]);
-                } else {
-                    $.get(url).then((function(d) {
-                        this.cache[url] = _.template(d);
-                        x.resolve(_.template(d));
-                    }).bind(this));
-                }
-                return x;
-            },
-            loadTemplate: function(name) {
-                return this.stream('./Templates/' + name + '.html');
-            },
-            initialize: function(options) {
-                this.options = options || {};
+    var x =  (typeof Parse !== "undefined") && ("Parse") || (typeof Backbone !== "undefined") && ("Backbone");
+    if(!x) return;
 
-                this.model && this.model.on("change", this.render.bind(this));
-                this.collection && this.collection.on("sync", this.render.bind(this));
-            },
-            render: function() {
-                var self = this;
-                this.loadTemplate(this.options.view || this.view).then(function(fn) {
-                    var d = self.model || self.collection;
-                    self.el.innerHTML = fn({
-                        data: d
-                    });
-                })
+    window[x].TemplateView = window[x].View.extend({
+        cache: {},
+        stream: function(url) {
+            var x = $.Deferred();
+            if (this.cache[url]) {
+                x.resolve(this.cache[url]);
+            } else {
+                $.get(url).then((function(d) {
+                    this.cache[url] = _.template(d);
+                    x.resolve(_.template(d));
+                }).bind(this));
             }
-        })
-    }
+            return x;
+        },
+        loadTemplate: function(name) {
+            return this.stream('./templates/' + name + '.html');
+        },
+        initialize: function(options) {
+            this.options = options || {};
 
-    if(typeof module !== "object"){
-        (window.Parse || window.Backbone)["TemplateView"] = TemplateView(window.Parse || window.Backbone, _, $)
-    }
+            this.model && this.model.on("change", this.render.bind(this));
+            this.collection && this.collection.on("add reset remove", this.render.bind(this));
+        },
+        render: function() {
+            var self = this;
+            this.loadTemplate(this.options.view || this.view).then(function(fn) {
+                var d = self.model || self.collection;
+                self.el.innerHTML = fn({
+                    data: d
+                });
+            })
+        }
+    })
 
-})(typeof module === "object" ? module.exports : window);
+})();
