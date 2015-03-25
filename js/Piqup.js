893bbf365fce9953634b9b432cf078751b02a944
Dorsh <dorshdeans@gmail.com>
2015-03-24 22:54:30 -0400

    geolocation/Google maps view

--- js/Piqup.js
+++ js/Piqup.js
@@ -32,28 +32,6 @@
         },
         signup: function()  {
             this.signupView.render().then(function(){
-                initAddMenu();
-                initSideMenu();
-                initTopMenu();
-                new WOW().init();
-            });
-        },
-        dashboard: function() {
-          this.dashboardView.render().then(function(){
-                initAddMenu();
-                initTopMenu();
-                new WOW().init();
-            });
-        },
-         games: function() {
-        this.gamesView.render().then(function(){
-                initSideMenu();
-                initTopMenu();
-                new WOW().init();
-            });
-        },
-        home: function() {
-            this.view.render().then(function(){
                 initSideMenu();
                 initTopMenu();
                 new WOW().init();
@@ -93,8 +71,64 @@
                 navigator.geolocation.getCurrentPosition(success, error);
 
             });
+        },
+        dashboard: function() {
+          this.dashboardView.render().then(function(){
+                initAddMenu();
+                initTopMenu();
+                new WOW().init();
+            });
+        },
+         games: function() {
+        this.gamesView.render().then(function(){
+                initSideMenu();
+                initTopMenu();
+                new WOW().init();
+            });
+        },
+        home: function() {
+            this.view.render().then(function(){
+                initSideMenu();
+                initTopMenu();
+                new WOW().init();
 
-        }
+        //         function success(pos) {
+        //             // pos.coords.latitude;
+        //             this.map = new GMaps ({
+        //                 el: "#map",
+        //                 lat: pos.coords.latitude,
+        //                 lng: pos.coords.longitude
+        //             })
+
+        //             this.map.addMarker({
+        //             lat: 29.637975,
+        //             lng: -95.395283,
+        //             title: 'Houston',
+
+        //             click: function(e) {
+        //                 alert('You clicked in this marker');
+        //             }
+        //         });
+
+        //               this.map.addMarker({
+        //             lat: 29.637975,
+        //             lng: -95.395283,
+        //             title: 'Houston Dynmao',
+
+        //             click: function(e) {
+        //                 alert('You clicked in this marker');
+        //             }
+        //         });
+        //         }
+        //         function error(err) {
+        //             console.log(err.code);
+        //         }
+
+        //         navigator.geolocation.getCurrentPosition(success, error);
+
+             });
+
+         }
     })
 
     Backbone.Session = Backbone.Model.extend({
